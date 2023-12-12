9502d6d7f738834ed46d2a660420a86697c2986e
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2023-12-12 18:01:27 +0530

    translate app using api

---
+++ translate-app/src/App.js
@@ -0,0 +1,93 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [options, setOptions] = useState([]);
+  const [to, setTo] = useState('es'); // Set default 'to' language
+  const [from, setFrom] = useState('en'); // Set default 'from' language
+  const [input, setInput] = useState('');
+  const [output, setOutput] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const translate = () => {
+    setLoading(true);
+
+    const params = new URLSearchParams();
+    params.append('q', input);
+    params.append('source', from);
+    params.append('target', to);
+    params.append('api_key', 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx');
+
+    axios.post('https://libretranslate.de/translate', params, {
+      headers: {
+        'accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    })
+    .then((res) => {
+      setOutput(res.data.translatedText);
+    })
+    .catch((error) => {
+      console.error('Translation error:', error);
+      // Handle error, display a message to the user, etc.
+    })
+    .finally(() => {
+      setLoading(false);
+    });
+  };
+
+  useEffect(() => {
+    axios.get('https://libretranslate.de/languages', {
+      headers: { accept: 'application/json' },
+    })
+    .then((res) => {
+      setOptions(res.data);
+    })
+    .catch((error) => {
+      console.error('Language options error:', error);
+      // Handle error, display a message to the user, etc.
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <div>
+        From ({from}) :
+        <select onChange={(e) => setFrom(e.target.value)}>
+          {options.map((opt) => (
+            <option key={opt.code} value={opt.code}>
+              {opt.name}
+            </option>
+          ))}
+        </select>
+        To ({to}) :
+        <select onChange={(e) => setTo(e.target.value)}>
+          {options.map((opt) => (
+            <option key={opt.code} value={opt.code}>
+              {opt.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <textarea
+          cols="50"
+          rows="8"
+          onInput={(e) => setInput(e.target.value)}
+        ></textarea>
+      </div>
+      <div>
+        <textarea cols="50" rows="8" value={output}></textarea>
+      </div>
+      <div>
+        <button onClick={translate} disabled={loading}>
+          {loading ? 'Translating...' : 'Translate'}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
